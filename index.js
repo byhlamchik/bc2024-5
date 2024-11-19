4fbe3b6bb7e3e319043396ea3ab0f15a3a721054
oleg <olegmazur541@gmail.com>
2024-11-20 00:55:18 +0200

    Додати обробку параметрів командного рядка

--- index.js
+++ index.js
@@ -3,3 +3,15 @@ const fs = require('fs');
 const path = require('path');
 const multer = require('multer');
 const { Command } = require('commander');
+const program = new Command();
+program
+    .requiredOption('-h, --host <host>', 'server host')
+    .requiredOption('-p, --port <port>', 'server port')
+    .requiredOption('-c, --cache <cache>', 'cache directory path');
+
+program.parse(process.argv);
+const { host, port, cache } = program.opts();
+
+if (!fs.existsSync(cache)) {
+    fs.mkdirSync(cache, { recursive: true });
+}
